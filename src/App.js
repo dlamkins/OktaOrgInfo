3c6ce87b892f6bf28aef65850ff60d5cbb2f2e51
Xander Lamkins <dade@lamkins.org>
2024-04-22 21:04:36 -0400

    Added better descriptions of the data it pulls.

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ const App = () => {
             <h1 className="text-center text-2xl font-bold text-indigo-600 sm:text-3xl">OktaOrgInfo</h1>
 
             <p className="mx-auto mt-4 max-w-md text-center text-gray-500">
-              This tool pulls basic information about your Okta tenant.
+              This tool will pull the OrgId, cell name, OIE vs. Classic, and more about the provided Okta tenant.
             </p>
 
             <div className="mb-0 mt-6 space-y-4 rounded-lg p-4 shadow-lg sm:p-6 lg:p-8">
